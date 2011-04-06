6f525670147ee1f15f05bd172e2321169a0b63fc
cmpilato@gmail.com <cmpilato@gmail.com@ca9da4be-2372-8728-6a4e-55d01d798f36>
2011-04-06 16:31:44 +0000

    * recurrence_lib/storage.py   New, event storage/persistance logic first pass.

---
+++ recurrence_lib/storage.py
@@ -0,0 +1,123 @@
+# ====================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ====================================================================
+
+"""storage.py:  Recurrence storage/persistance routines."""
+
+
+import events
+
+LATEST_VERSION = 1
+
+
+def parse_data_file_v1(fp):
+  definitions = {}
+  occurrences = []
+
+  def unescape_piece(piece):
+    piece = piece.replace('\\r', '\r')
+    piece = piece.replace('\\n', '\n')
+    piece = piece.replace('\\t', '\t')
+    piece = piece.replace('\\\\', '\\')
+    return piece
+  
+  while 1:
+    line = fp.readline()
+    if not line:
+      break
+    line = line.rstrip('\n\r')
+    pieces = map(lambda x: unescape_piece(x), line.split('\t'))
+    if pieces[0] == 'EventDefinition':
+      ed = events.EventDefinition()
+      ed.set_uuid(pieces[1])
+      ed.set_description(pieces[2])
+      ed.set_start_date(int(pieces[3]))
+      if len(pieces) > 4:
+        er = events.EventRecurrence()
+        er.set_until_date(int(pieces[4]))
+        er.set_period(events.period_from_string(pieces[5]))
+        ed.set_recurrence(er)
+      definitions[ed.get_uuid()] = ed
+    elif pieces[0] == 'EventOccurrence':
+      eo = events.EventOccurrence()
+      eo.set_definition(definitions[pieces[1]])
+      eo.set_date(int(pieces[2]))
+      eo.set_cleared(pieces[3] == 'true' and True or False)
+      occurrences.append(eo)
+    else:
+      raise Exception("Unrecognized record type.")
+  return definitions.values(), occurrences
+
+      
+def read_data_file(filepath):
+  """Parse a Recurrence data file, returning"""
+  version = None
+  fp = open(filepath, 'r')
+  try:
+    version_line = fp.readline().rstrip('\n\r')
+    if version_line.startswith('#version = '):
+      version = int(version_line[11:])
+    if version == 1:
+      return parse_data_file_v1(fp)
+    else:
+      raise Exception("Unrecognized data file format for file '%s'."
+                      % (filepath))
+  except:
+    raise
+  
+
+def unparse_date_file_v1(filepath, definitions, occurrences):
+
+  def escape_piece(piece):
+    piece = str(piece)
+    piece = piece.replace('\\', '\\\\')
+    piece = piece.replace('\t', '\\t')
+    piece = piece.replace('\n', '\\n')
+    piece = piece.replace('\r', '\\r')
+    return piece
+  
+  def unparse_pieces(pieces):
+    return '\t'.join(map(lambda x: escape_piece(x), pieces)) + '\n'
+
+  def definition_to_pieces(definition):
+    pieces = ['EventDefinition',
+              definition.get_uuid(),
+              definition.get_description(),
+              definition.get_start_date(),
+              ]
+    recurrence = definition.get_recurrence()
+    if recurrence:
+      pieces.extend([recurrence.get_until_date(),
+                     events.period_to_string(recurrence.get_period()),
+                     ])
+    return pieces
+
+  def occurrence_to_pieces(occurrence):
+    return ['EventOccurrence',
+            occurrence.get_definition().get_uuid(),
+            occurrence.get_date(),
+            occurrence.get_cleared() and 'true' or 'false',
+            ]
+    
+  fp = open(filepath, 'w')
+  fp.write('#version = 1\n')
+  for definition in definitions:
+    fp.write(unparse_pieces(definition_to_pieces(definition)))
+  for occurrence in occurrences:
+    fp.write(unparse_pieces(occurrence_to_pieces(occurrence)))
+  fp.close()
+
+
+def write_data_file(filepath, definitions, occurrences, version=LATEST_VERSION):
+  assert(version == LATEST_VERSION)
+  unparse_date_file_v1(filepath, definitions, occurrences)
